63ef45e96e0af906f66d45f551a452ee9ecf7982
Ramos <luizramos128@hotmail.com>
2024-11-12 22:35:09 -0300

    Refinando sua tecnica com desafios de codigo basicos em logica

---
+++ Desafios de codigo basicos em Logica/capturandoPokemons.js
@@ -0,0 +1,43 @@
+let escolhaDoTreinador = 4 
+let pokemonEscolhido
+
+
+if ( escolhaDoTreinador === 1) {
+    pokemonEscolhido = "Bulbasaur"
+} else if ( escolhaDoTreinador ===  2) {
+    pokemonEscolhido = "Charmander"
+} else if ( escolhaDoTreinador === 4) {
+    pokemonEscolhido = "Pikachu"
+} else{
+    pokemonEscolhido = "Mewtwo"
+}
+
+if (pokemonEscolhido) {
+    console.log("Voce escolheu o " + pokemonEscolhido + " como seu Pokemon inicial.")
+}
+
+
+
+/*
+
+Descrição
+No mundo dos jogos Pokémon, os treinadores começam sua jornada escolhendo um dos três Pokémons iniciais: Bulbasaur, Charmander e Mewtwo. Cada treinador escolhe um dos quatro pokemons. Seu desafio é criar uma solução que permita ao jogador escolher um dos Pokémons iniciais e exibir uma mensagem de boas-vindas e o Pokémon escolhido.
+
+Entrada
+Você receberá um número inteiro que representa a escolha do treinador: 1 para Bulbasaur, 2 para Charmander, 4 Pikachu e 5 para Mewtwo.
+
+Saída
+A saída deve ser uma mensagem de boas-vindas que inclua o nome do Pokémon escolhido.
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+Entrada	Saída
+Escolha o seu Pokemon: 1	Voce escolheu o Bulbasaur como seu Pokemon inicial.
+Escolha o seu Pokemon: 2	Voce escolheu o Charmander como seu Pokemon inicial.
+Escolha o seu Pokemon: 4	Voce escolheu o Pikachu como seu Pokemon inicial.
+Escolha o seu Pokemon: 5	Voce escolheu o Mewtwocomo seu Pokemon inicial.
+
+
+
+*/
